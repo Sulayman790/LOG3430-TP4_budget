d1774666aeff2299bf134fb730120a8f2c44b268
Sulayman790 <51691536+Sulayman790@users.noreply.github.com>
2024-03-22 08:49:44 -0400

    add myScrit.py

--- budget/myScript.py
+++ budget/myScript.py
@@ -1,4 +1,8 @@
 import os
 
-os.system('git bisect start c1a4be04b972b6c17db242fc37752ad517c29402 e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
+bad_git_bisect = 'c1a4be04b972b6c17db242fc37752ad517c29402'
+good_git_bisect = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
+
+os.system(f'git bisect start {bad_git_bisect} {good_git_bisect}')
 os.system('git bisect run python manage.py test')
+os.system('git bisect reset')
